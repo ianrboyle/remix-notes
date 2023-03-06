b178a74ecc56d4e3debfafe30f3317905874cb1a
Ian Boyle <irboyle21@gmail.com>
2023-03-06 11:52:08 -0500

    adds loader  data

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,15 +1,40 @@
-import { Link } from "@remix-run/react";
-import NewNote , {links as NewNoteLinks} from "~/components/notes/NewNote";
-import type { LinksFunction } from "@remix-run/node";
+import NewNote , {links as newNoteLinks} from "~/components/notes/NewNote";
+import type { ActionFunction, LinksFunction, LoaderFunction } from "@remix-run/node";
+import NoteList, {links as noteListLinks} from "~/components/notes/NoteList";
+import { getStoredNotes, storeNotes } from "~/data/notes";
+import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export default function NotesPage() {
+  const notes = useLoaderData();
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
 }
 
+export const  action: ActionFunction = async ({request}) => {
+  const formData = await request.formData();
+  // const noteDate = {
+  //   title: formData.get("title"),
+  //   content: formData.get("content")
+  // }
+  const noteData = Object.fromEntries(formData);
+  // add validation
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+  return redirect('/notes')
+}
+
+export const loader: LoaderFunction = async () => {
+  const notes = await getStoredNotes();
+  // return json(notes)
+  return notes;
+  }
 export const links: LinksFunction = () => {
-  return [...NewNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
 }
